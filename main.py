d7fd9827bd018658d1ca4c7e5ccfa8bc341b28bb
Danil Lapin <lapindanil22@gmail.com>
2023-12-04 16:21:14 +0700

    Add GET /currency/* endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 import sqlite3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -6,15 +7,15 @@ hostName = "localhost"
 serverPort = 8080
 
 
-def exchange_rates_list_to_json(exchange_rates):
+def exchange_rates_tuple_to_list_of_dicts(exchange_rates):
     exchange_rates_list = []
     for exchange_rate in exchange_rates:
         base_currency_tuple = cursor.execute("SELECT * FROM currency WHERE id = ?",
                                              (exchange_rate[1],)).fetchone()
-        base_currency_json = currencies_list_to_json([base_currency_tuple])
+        base_currency_json = currencies_tuple_to_list_of_dicts([base_currency_tuple])
         target_currency_tuple = cursor.execute("SELECT * FROM currency WHERE id = ?",
                                                (exchange_rate[2],)).fetchone()
-        target_currency_json = currencies_list_to_json([target_currency_tuple])
+        target_currency_json = currencies_tuple_to_list_of_dicts([target_currency_tuple])
 
         exchange_rate_dict = {
             "id": exchange_rate[0],
@@ -26,8 +27,8 @@ def exchange_rates_list_to_json(exchange_rates):
     return exchange_rates_list
 
 
-def currencies_list_to_json(currencies):
-    currencies_list = []
+def currencies_tuple_to_list_of_dicts(currencies):
+    currencies_dict = []
     for currency in currencies:
         currency_dict = {
             "id": currency[0],
@@ -35,16 +36,20 @@ def currencies_list_to_json(currencies):
             "fullName": currency[2],
             "sign": currency[3],
         }
-        currencies_list.append(currency_dict)
-    return currencies_list
+        currencies_dict.append(currency_dict)
+    return currencies_dict
 
 
 class MyServer(BaseHTTPRequestHandler):
+
     def do_GET(self):
         if self.path == '/api/currencies':
             self.get_currencies()
         elif self.path == '/api/exchangeRates':
             self.get_exchange_rates()
+        elif re.search("/api/currency/.+", self.path):
+            currency = self.path.split("/")[-1]
+            self.get_currency(currency)
         else:
             self.send_response(400)
             self.end_headers()
@@ -52,22 +57,31 @@ class MyServer(BaseHTTPRequestHandler):
     def get_exchange_rates(self):
         cursor.execute("SELECT * FROM exchange_rate")
         exchange_rates = cursor.fetchall()
-        exchange_rates_list = exchange_rates_list_to_json(exchange_rates)
+        exchange_rates_list_of_dicts = exchange_rates_tuple_to_list_of_dicts(exchange_rates)
 
+        self.send_200_json_headers()
+        self.wfile.write(bytes(json.dumps(exchange_rates_list_of_dicts, indent=2), "utf-8"))
+
+    def send_200_json_headers(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(bytes(json.dumps(exchange_rates_list, indent=2), "utf-8"))
 
     def get_currencies(self):
         cursor.execute("SELECT * FROM currency")
         currencies = cursor.fetchall()
-        currencies_list = currencies_list_to_json(currencies)
+        currencies_list_of_dicts = currencies_tuple_to_list_of_dicts(currencies)
 
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(currencies_list, indent=2), "utf-8"))
+        self.send_200_json_headers()
+        self.wfile.write(bytes(json.dumps(currencies_list_of_dicts, indent=2), "utf-8"))
+
+    def get_currency(self, currency_code):
+        cursor.execute("SELECT * FROM currency WHERE code = ?", (currency_code,))
+        currency = cursor.fetchone()
+        currency_list_of_dict = currencies_tuple_to_list_of_dicts([currency])
+
+        self.send_200_json_headers()
+        self.wfile.write(bytes(json.dumps(currency_list_of_dict, indent=2), "utf-8"))
 
 
 def init_database_if_not_exists():
